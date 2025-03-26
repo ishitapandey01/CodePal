8ed7f4905fc1524ab8678a7c90bc89875a538570
Ishita Pandey <ishitapandey@Ishitas-MacBook-Air.local>
2025-03-26 18:55:10 +0530

    Fixed unstaged changes in backend

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ const app = require('./src/app')
 
 
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
-})
+// app.listen(3000, () => {
+//     console.log('Server is running on http://localhost:3000')
+// })
+const PORT = process.env.PORT || 1000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
